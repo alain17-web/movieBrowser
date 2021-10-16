80124f6b1b904e6828f2f4a4ad525c0e162b0c7b
alain17-web <web2020.alain@gmail.com>
2021-10-16 18:35:01 +0200

    Detail

--- movie_browser/src/components/Detail.js
+++ movie_browser/src/components/Detail.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import MovieDetail from './MovieDetail';
-import Nav from './Nav';
+
 
 
 const Detail = () => {
     return ( 
         <div>
             <MovieDetail/>
-            <Nav/>
+            
         </div>
         
      );
--- movie_browser/src/components/MovieDetail.js
+++ movie_browser/src/components/MovieDetail.js
@@ -3,6 +3,9 @@ import DetailImg from './detailImg.png';
 import './MovieDetail.scss';
 import { FiClock } from "react-icons/fi";
 import { AiOutlineStar } from "react-icons/ai";
+import  Skywalker from './skywalker.png';
+import  Force  from './force.png';
+import  Half  from './half.png';
 
 const MovieDetail = () => {
     return ( 
@@ -17,7 +20,7 @@ const MovieDetail = () => {
                 <p>4K</p>
             </div>
             <div className="info">
-                <p><FiClock/> 152 minutes   <AiOutlineStar/> 7.0 (MDb)</p>
+                <p><FiClock/> 152 minutes   <AiOutlineStar/> 7.0</p>
             </div>
             <div className="release">
                 <h4>Release date</h4>
@@ -25,7 +28,31 @@ const MovieDetail = () => {
             </div>
             <div className="genre">
                 <h4>Genre</h4>
+                <div className="genres">
+                    <p>Action</p>
+                    <p>SciFi</p>
+                </div>
             </div>
+            <div className="synopsis">
+                <h4>Synopsis</h4>
+                <p>Rey (Daisy Ridley) finally manages to find the legendary Jedi knight, Luke Skywalker (Mark Hamill) on an island with a magical aura. The heroes of The Force Awakens including Leia, Finn <span>Read more.</span></p>
+            </div>
+            <div className="related">
+                <h4>Related Movies</h4>
+                <div className="images">
+                    <figure>
+                        <img src={Skywalker} alt="skywalker" />
+                    </figure>
+                    <figure>
+                        <img src={Force} alt="force" />
+                    </figure>
+                    <figure>
+                        <img src={Half} alt="half" />
+                    </figure>
+                </div>
+            </div>
+
+
         </div>
      );
 }
